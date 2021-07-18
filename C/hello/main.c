435697722f9c5babb3f6ce9229437a535f5914ab
fireflower0 <fireflower0@hotmail.co.jp>
2021-07-18 10:01:46 +0900

    Fixed a hello in C

--- C/hello/main.c
+++ C/hello/main.c
@@ -6,8 +6,21 @@ int main(int argc, char* argv[]) {
 
   if (argc != 2) {
     printf("Error: argument number\n");
-    printf("argc: %d\n", argc);
     return ERROR_ARGUMENT_NUMBER;
+  } else {
+    if (argv[1][0] == '-') {
+      switch (argv[1][1]) {
+      case 'v':
+        printf("Version: 0.0.1\n");
+        return SUCCESS;
+      case 'h':
+        printf("[Help]\n"
+               "Usage: ./hello <your name>\n"
+               "v : Show version\n"
+               "h : Show help message\n");
+        return SUCCESS;
+      }
+    }
   }
 
   printf("Hello, %s!\n", argv[1]);
